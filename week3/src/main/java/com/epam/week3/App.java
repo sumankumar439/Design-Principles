7079dca028e8876954a33fe3f6b0eb713833443c
sumankumar439 <61882890+sumankumar439@users.noreply.github.com>
2020-07-25 18:11:51 +0530

    Add files via upload

---
+++ week3/src/main/java/com/epam/week3/App.java
@@ -0,0 +1,26 @@
+package com.epam.week3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public int addition(int a,int b)
+	{
+		return a+b;
+	}
+	public  int subtraction(int a,int b)
+	{
+		return a-b;
+	}
+	public int multiplication(int a,int b)
+	{
+		return a*b;
+	}
+	public int division(int a,int b)
+	{
+		return a/b;
+	}
+
+}
